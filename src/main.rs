2e8c56ef0a58b3cd7363d9df76c5a3a88c8c1e02
jq-rs <jq-rs@mles.io>
2023-11-24 00:20:17 +0100

    Some advancements.

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ use std::net::Ipv6Addr;
 use std::path::PathBuf;
 use tokio_stream::wrappers::TcpListenerStream;
 use tokio::sync::mpsc;
-use tokio::sync::mpsc::UnboundedSender;
+use tokio::sync::oneshot;
 use tokio::sync::mpsc::Sender;
 use warp::Filter;
 use warp::Error;
@@ -20,13 +20,14 @@ use futures_util::{StreamExt, SinkExt};
 use futures_util::FutureExt;
 use serde::{Deserialize, Serialize};
 //use serde_json::Result;
-use std::sync::{Arc,Mutex};
 use std::collections::HashMap;
+use std::collections::VecDeque;
 use siphasher::sip::SipHasher;
 use tokio_stream::wrappers::UnboundedReceiverStream;
 use tokio_stream::wrappers::ReceiverStream;
-use futures_util::TryFutureExt;
 use std::hash::{Hash, Hasher};
+use std::sync::atomic::{AtomicU64, Ordering};
+use std::sync::Arc;
 
 #[derive(Serialize, Deserialize, Hash)]
 struct MlesHeader {
@@ -36,7 +37,7 @@ struct MlesHeader {
 
 #[derive(Parser, Debug)]
 struct Args {
-    /// Domains
+    /// Domain
     #[clap(short, required = true)]
     domains: Vec<String>,
 
@@ -64,13 +65,21 @@ struct Args {
 const ACCEPTED_PROTOCOL: &str = "mles-websocket";
 const TASK_BUF: usize = 16;
 const WS_BUF:usize = 128;
+const HISTORY_LIMIT:usize = 3000;
+
+#[derive(Debug)]
+enum WsEvent {
+    Init(u64, u64, Sender<Result<Message, warp::Error>>, oneshot::Sender<u64>, Message),
+    Msg(u64, u64, Message),
+    Logoff(u64, u64)
+}
 
 #[tokio::main]
 async fn main() {
     simple_logger::init_with_level(log::Level::Info).unwrap();
     let args = Args::parse();
 
-    let (tx, rx) = mpsc::channel::<(u64, Sender<Result<Message, warp::Error>>, Result<Message, warp::Error>)>(TASK_BUF);
+    let (tx, rx) = mpsc::channel::<WsEvent>(TASK_BUF);
     let mut rx = ReceiverStream::new(rx);
     let tcp_listener = tokio::net::TcpListener::bind((Ipv6Addr::UNSPECIFIED, args.port)).await.unwrap();
     let tcp_incoming = TcpListenerStream::new(tcp_listener);
@@ -82,18 +91,63 @@ async fn main() {
         .tokio_incoming(tcp_incoming, Vec::new());
 
     tokio::spawn(async move {
-        let mut db = HashMap::new();
-        while let Some((h, tx2, Ok(msg))) = rx.next().await {
-            println!("Got remote message {:?}", msg);
+        let mut msg_db: HashMap<u64, VecDeque<Message>> = HashMap::new();
+        let mut uid_db: HashMap<(u64, u64), Sender<Result<Message, warp::Error>>> = HashMap::new();
+        while let Some(event) = rx.next().await {
+            match event {
+                WsEvent::Init(h, ch, tx2, err_tx, msg) => {
+                    if !uid_db.contains_key(&(h, ch)) {
+                        if !msg_db.contains_key(&ch) {
+                            msg_db.insert(ch, VecDeque::with_capacity(HISTORY_LIMIT));
+                        }
+                        for (_, tx) in &uid_db {
+                            tx.send(Ok(msg.clone())).await;
+                        }
+                        uid_db.insert((h, ch), tx2.clone());
+                        println!("Added {h}");
+                        err_tx.send(h);
+
+                        let mut queue = msg_db.get_mut(&ch).unwrap();
+                        for qmsg in &mut *queue {
+                            tx2.send(Ok(qmsg.clone())).await;
+                        }
+                        if queue.len() == HISTORY_LIMIT {
+                            queue.pop_front();
+                        }
+                        queue.push_back(msg);
+                    }
+                },
+                WsEvent::Msg(h, ch, msg) => {
+                    for (_, tx) in uid_db.iter().filter(|(&(x,_),_)| x != h) {
+                        tx.send(Ok(msg.clone())).await;
+                    }
+                    let mut queue = msg_db.get_mut(&ch).unwrap();
+                    if queue.len() == HISTORY_LIMIT {
+                        queue.pop_front();
+                    }
+                    queue.push_back(msg);
+                },
+                WsEvent::Logoff(h, ch) => {
+                    uid_db.remove(&(h, ch));
+                    println!("Removed {h}");
+                },
+            }
+            /*println!("Got remote message {:?}", event);
+            if db.contains_key(&h) && tx2.is_some() {
+                let reason = format!("Existing client");
+            }
             if !db.contains_key(&h) {
-                let msg_vec = vec![msg.clone()];
-                db.insert(h, msg_vec);
+                let err_tx = err_tx.unwrap();
+                err_tx.send(h);
+                //Send history data here
+                let msg_vec = VecDeque::with_capacity(HISTORY_LIMIT);
+                db.insert(h, (tx2, msg_vec));
             }
             else {
-                let mut msg_vec = db.get_mut(&h).unwrap();
+                let (_, ref mut msg_vec) = db.get_mut(&h).unwrap();
                 msg_vec.push(msg.clone());
-            }
-            tx2.send(Ok(msg)).await;
+                //Forward to others here
+            }*/
         }
     });
     
@@ -109,36 +163,68 @@ async fn main() {
             let tx_inner = tx_clone.clone();
             ws.on_upgrade(move |websocket| {
                 let (tx2, rx2) = mpsc::channel::<Result<Message, warp::Error>>(WS_BUF);
+                let (err_tx, err_rx) = oneshot::channel();
                 let mut rx2 = ReceiverStream::new(rx2);
                 let (ws_tx, mut ws_rx) = websocket.split();
-                let tx = tx_inner.clone();
+                let h: Arc<AtomicU64> = Arc::new(AtomicU64::new(0));
+                let ch: Arc<AtomicU64> = Arc::new(AtomicU64::new(0));
 
                 println!("Listening mles-websocket...");
-
+                let tx = tx_inner.clone();
+                let h_clone = h.clone();
+                let ch_clone = ch.clone();
                 tokio::spawn(async move {
-                    let mut h = 0;
+                    let h = h_clone;
+                    let ch = ch_clone;
                     if let Some(Ok(msg)) = ws_rx.next().await {
+                        if !msg.is_text() {
+                            return;
+                        }
                         let msghdr: Result<MlesHeader, serde_json::Error> = serde_json::from_str(msg.to_str().unwrap());
                         match msghdr {
                             Ok(msghdr) => {
                                 println!("Got fine msghdr!");
                                 let mut hasher = SipHasher::new();
                                 msghdr.hash(&mut hasher);
-                                h = hasher.finish();
-                                tx.send((h, tx2.clone(), Ok(msg))).await;
+                                h.store(hasher.finish(), Ordering::SeqCst);
+                                let hasher = SipHasher::new();
+                                ch.store(hasher.hash(&msghdr.channel.as_bytes()), Ordering::SeqCst);
+                                tx.send(WsEvent::Init(h.load(Ordering::SeqCst), ch.load(Ordering::SeqCst), tx2, err_tx, msg)).await;
                             },
                             Err(_) => return
                         }
                     }
+                    match err_rx.await {
+                        //Client ok?
+                        Ok(v) => {
+                            println!("got = {:?}", v);
+                        },
+                        Err(_) => {
+                            println!("the sender dropped");
+                            return;
+                        }
+                    }
                     while let Some(Ok(msg)) = ws_rx.next().await {
-                        tx.send((h, tx2.clone(), Ok(msg))).await;
+                        tx.send(WsEvent::Msg(h.load(Ordering::SeqCst), ch.load(Ordering::SeqCst), msg)).await;
                     }
                 });
 
+                let tx_clone = tx_inner.clone();
                 rx2.forward(ws_tx)
-                    .map(|res| {
+                    .map( move |res| {
+                        let tx = tx_clone.clone();
                         if let Err(e) = res {
-                            eprintln!("Websocket error: {:?}", e);
+                            println!("Websocket error: {:?}", e);
+                        }
+                        else {
+                            println!("Websocket: {:?}", res);
+                        }
+                        let h = h.load(Ordering::SeqCst);
+                        let ch = ch.load(Ordering::SeqCst);
+                        if h != 0 && ch != 0 {
+                            tokio::spawn(async move {
+                                tx.send(WsEvent::Logoff(h, ch)).await;
+                            });
                         }
                     })
                 /* TODO
